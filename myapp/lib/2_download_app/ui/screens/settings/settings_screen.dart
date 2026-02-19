d41c0857b3f1fc41e26a8fec2e355321c3f162c6
BineyIsMe <kingpanha5@gmail.com>
2026-02-19 16:22:58 +0700

    task2-downloader-theme color complete

--- myapp/lib/2_download_app/ui/screens/settings/settings_screen.dart
+++ myapp/lib/2_download_app/ui/screens/settings/settings_screen.dart
@@ -1,52 +1,82 @@
-
 import 'package:flutter/material.dart';
 
 import '../../providers/theme_color_provider.dart';
 import '../../theme/theme.dart';
 import 'widget/theme_color_button.dart';
 
+class ColorNotifier extends ChangeNotifier {
+  ThemeColor _color = ThemeColor.blue;
+  ThemeColor get color => _color;
+  void onChangeColor(ThemeColor color) {
+    switch (color) {
+      case ThemeColor.blue:
+        _color = ThemeColor.blue;
+        break;
+      case ThemeColor.green:
+        _color = ThemeColor.green;
+        break;
+      case ThemeColor.pink:
+        _color = ThemeColor.pink;
+        break;
+    }
+    notifyListeners();
+  }
+}
+
+ColorNotifier myColorNotifier = ColorNotifier();
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
- 
+  void onChangeCard(ThemeColor color) {
+    myColorNotifier.onChangeColor(color);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: currentThemeColor.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text(
-            "Settings",
-            style: AppTextStyles.heading.copyWith(
-              color: currentThemeColor.color,
-            ),
+    return ListenableBuilder(
+      listenable: myColorNotifier,
+      builder: (context, _) {
+        return Container(
+          color: myColorNotifier.color.backgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Settings",
+                style: AppTextStyles.heading.copyWith(
+                  color: myColorNotifier.color.color,
+                ),
+              ),
+        
+              SizedBox(height: 50),
+        
+              Text(
+                "Theme",
+                style: AppTextStyles.label.copyWith(color: AppColors.textLight),
+              ),
+        
+              SizedBox(height: 10),
+        
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: ThemeColor.values
+                    .map(
+                      (theme) => ThemeColorButton(
+                        themeColor: theme,
+                        
+                        isSelected: theme == myColorNotifier.color,
+                        onTap: (value) {
+                          onChangeCard(theme);
+                        },
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
           ),
-
-          SizedBox(height: 50),
-
-          Text(
-            "Theme",
-            style: AppTextStyles.label.copyWith(color: AppColors.textLight),
-          ),
-
-          SizedBox(height: 10),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: ThemeColor.values
-                .map(
-                  (theme) => ThemeColorButton(
-                    themeColor: theme,
-                    isSelected: theme == currentThemeColor,
-                    onTap: (value) { },
-                  ),
-                )
-                .toList(),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
- 
